46a24dd82fe9041b2347d13f56db457dcf13f05d
Daniel Kostro <kostro.d@gmail.com>
2018-08-23 18:02:13 +0200

    remove gulp nsp

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,18 @@ var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
-var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 
-gulp.task('static', function () {
+gulp.task('static', function() {
   return gulp
     .src('generators/app/*.js')
     .pipe(excludeGitignore())
-    .pipe(eslint({fix: true}))
+    .pipe(eslint({ fix: true }))
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
-});
-
-gulp.task('pre-test', function () {
+gulp.task('pre-test', function() {
   return gulp
     .src('app/*.js')
     .pipe(excludeGitignore())
@@ -33,25 +28,24 @@ gulp.task('pre-test', function () {
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['pre-test'], function (cb) {
+gulp.task('test', ['pre-test'], function(cb) {
   var mochaErr;
 
   gulp
     .src('test/**/*.js')
     .pipe(plumber())
-    .pipe(mocha({reporter: 'spec'}))
-    .on('error', function (err) {
+    .pipe(mocha({ reporter: 'spec' }))
+    .on('error', function(err) {
       mochaErr = err;
     })
     .pipe(istanbul.writeReports())
-    .on('end', function () {
+    .on('end', function() {
       cb(mochaErr);
     });
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
   gulp.watch(['app/*.js', 'test/**'], ['test']);
 });
 
-gulp.task('prepublish', ['nsp']);
 gulp.task('default', ['static', 'test']);
