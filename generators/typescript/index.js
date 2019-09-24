dc2f6cd423c7e273c0bce988109dc11d7bddacaa
Michaël Zasso <targos@protonmail.com>
2019-09-24 17:19:56 +0200

    fix: make tsconfig compatible with ESLint plugin

--- generators/typescript/index.js
+++ generators/typescript/index.js
@@ -88,6 +88,10 @@ module.exports = class extends Generator {
       this.templatePath('tsconfig.json'),
       this.destinationPath('tsconfig.json'),
     );
+    this.fs.copy(
+      this.templatePath('tsconfig.cjs.json'),
+      this.destinationPath('tsconfig.cjs.json'),
+    );
     if (includes.notOnlyNode) {
       this.fs.copy(
         this.templatePath('tsconfig.esm.json'),
