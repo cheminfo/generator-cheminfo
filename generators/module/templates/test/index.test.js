a1cdc0acc60e9040766f7d8ff4a761a448fd04d0
Luc Patiny <luc@patiny.com>
2024-08-05 11:29:05 +0200

    chore: fix order and empty line

--- generators/module/templates/test/index.test.js
+++ generators/module/templates/test/index.test.js
@@ -1,5 +1,6 @@
+import { expect, test } from 'vitest';
+
 import { myModule } from '..';
-import { test, expect } from 'vitest';
 
 test('Should return 42', () => {
   expect(myModule()).toStrictEqual(42);
